f42d62f2a1f27b8c51280a5cd87f4e2c1b48fc02
SirhcPaul <christopher.dobson14@gmail.com>
2024-12-18 14:57:24 +0000

    task 11 complete

--- src/components/ArticleCards.jsx
+++ src/components/ArticleCards.jsx
@@ -1,12 +1,21 @@
 import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
+
 
 export default function ArticleCards ({ articles }) {
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return format(date, 'dd MMM yyyy');
+    };
+
     return (
         <ul id="articles-list">
             {articles.map((article) => (
                 <li key={article.article_id} className={`article-card ${article.topic}`}>
                     <Link to={`/articles/${article.article_id}`} className='article-card-link'>
                     <h3>{article.title}</h3>
+                    <p>{formatDate(article.created_at)}</p>
+                    <p>Comments: {article.comment_count} Votes: {article.votes}</p>
                     </Link>
                     </li>
             ))}
